f043a94e2022cabdaec10478355a6d23bdb88b0c
Laitas <marciukash@gmail.com>
2021-12-20 14:15:19 +0200

    i really need to stop committing before I test things.

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -20,14 +20,14 @@ const Date = ({ className, date, preview = false }) => {
   const month = isNaN(date.slice(5, 7) - 1)
     ? date.slice(5, 6) - 1
     : date.slice(5, 7) - 1;
-  const day = date.slice(8, date.length).length <= 1 
-    ? date.slice(7, date.length) 
-    : date.slice(8, date.length);
-    //Above code is used to remove hyphens from date e.g 2021-12-12 to 2021 12 12
+  const day = date.slice(7, date.length).includes('-')
+      ? date.slice(8, date.length)
+      : date.slice(7, date.length);
+  // Above code is used to remove hyphens from date e.g 2021-12-12 to 2021 12 12
   return (
     <span className={className}>
       {preview && "Due"}{" "}
-      {`${year} ${monthNames[month]} ${day.length < 2 ? "0" + day : day}`}
+      {`${year} ${monthNames[month]} ${day?.length < 2 ? "0" + day : day}`}
     </span>
   );
 };
