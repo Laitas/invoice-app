55723ce7d4665b196060b424f9c1d1c6576cfa63
Laitas <marciukash@gmail.com>
2021-12-23 16:31:20 +0200

    Created user dropdown and implemented user image upload functionality

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,53 +1,140 @@
-import React , {useState, useEffect} from 'react'
-import Logo from '../../assets/logo.svg'
-import IconMoon from '../../assets/icon-moon.svg'
-import IconSun from '../../assets/icon-sun.svg'
-import Userimg from '../../assets/image-avatar.jpg'
-import { useSelector, useDispatch } from 'react-redux'
-import { auth } from '../../firebase'
-import { setInvoices, setUser } from '../../redux/userSlice'
-import { signOut } from '@firebase/auth'
-import './Nav.scss'
+import React, { useState, useEffect } from "react";
+import Logo from "../../assets/logo.svg";
+import IconMoon from "../../assets/icon-moon.svg";
+import IconSun from "../../assets/icon-sun.svg";
+import Userimg from "../../assets/image-avatar.jpg";
+import { useSelector, useDispatch } from "react-redux";
+import { auth, storage, db } from "../../firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { setInvoices, setUser } from "../../redux/userSlice";
+import { signOut } from "@firebase/auth";
+import "./Nav.scss";
 const Nav = () => {
-    const [theme,toggleTheme] = useState(false)
-    const user = useSelector(state => state.user.user)
-    const dispatch = useDispatch()
-    const userSignOut = () =>{
-        signOut(auth);
-        dispatch(
-          setUser({
-            photoURL: "",
-            email: "",
-            displayName: "",
-            uid: "",
-          })
-        );
-        dispatch(setInvoices([]))
-    }
-    useEffect(()=>{
-        const body = document.body
-        if(theme && body.classList.contains('dark-theme')){
-            body.classList.remove('dark-theme')
-        }else{
-            body.classList.add('dark-theme')
-        }
-    },[theme])
-    return (
-        <nav className="nav">
-            <div className="nav--logo">
-            <img src={Logo} alt="" />
-            <div className="nav--logo-whateverthatis"></div>
-            </div>
-            <div className="nav--bottom">
-                <div className="nav--bottom-theme-toggle">
-                    <img src={theme ? IconMoon : IconSun} onClick={()=> toggleTheme(!theme)} alt="theme toggle" />
-                </div>
-                <div onClick={userSignOut} className="nav--bottom-user">
-                    <img src={user && user.photoURL ? user.photoURL : Userimg} alt="user" />
-                </div>
-            </div>
-        </nav>
-    )
-}
+  const [theme, toggleTheme] = useState(false);
+  const [dropdown, toggleDropdown] = useState(false);
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
 
-export default Nav
+  const handleImageUpload = async (e) => {
+    if (e.target.files[0]) {
+      const storageRef = ref(storage, `Images/${e.target.files[0].name}`);
+      const uploadTask = uploadBytesResumable(storageRef, e.target.files[0]);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then((url) => {
+              dispatch(
+                setUser({
+                  photoURL: url,
+                  email: user.email,
+                  displayName: user.displayName,
+                  uid: user.uid,
+                })
+              );
+              updateDoc(doc(db, "users", user.key), {
+                photoURL: url,
+              });
+            })
+            .catch((e) => console.log(e));
+        }
+      );
+    } else {
+      return;
+    }
+  };
+  const userSignOut = () => {
+    signOut(auth);
+    dispatch(
+      setUser({
+        photoURL: "",
+        email: "",
+        displayName: "",
+        uid: "",
+      })
+    );
+    dispatch(setInvoices([]));
+  };
+  useEffect(() => {
+    const body = document.body;
+    if (theme && body.classList.contains("dark-theme")) {
+      body.classList.remove("dark-theme");
+    } else {
+      body.classList.add("dark-theme");
+    }
+  }, [theme]);
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav--logo">
+          <img src={Logo} alt="" />
+          <div className="nav--logo-whateverthatis"></div>
+        </div>
+        <div className="nav--bottom">
+          <div className="nav--bottom-theme-toggle">
+            <img
+              src={theme ? IconMoon : IconSun}
+              onClick={() => toggleTheme(!theme)}
+              alt="theme toggle"
+            />
+          </div>
+          {user.uid ? (
+            <div
+              onClick={() => toggleDropdown(!dropdown)}
+              className="nav--bottom-user"
+            >
+              <img
+                src={user && user.photoURL ? user.photoURL : Userimg}
+                alt="user"
+              />
+            </div>
+          ) : (
+            <div className="nav--bottom-user">
+              <img
+                src={user && user.photoURL ? user.photoURL : Userimg}
+                alt="user"
+              />
+            </div>
+          )}
+        </div>
+      </nav>
+      <div
+        className={`nav--dropdown ${
+          dropdown ? "nav--dropdown-active" : "nav--dropdown-hidden"
+        }`}
+      >
+        <ul>
+          <li>
+            <label>
+              <input
+                type="file"
+                name="avatar"
+                id="avatar"
+                onChange={(e) => {
+                  handleImageUpload(e);
+                }}
+                accept="image/png, image/jpeg"
+              />
+              Upload picture
+            </label>
+          </li>
+          <li
+            onClick={() => {
+              toggleDropdown(false);
+              userSignOut();
+            }}
+          >
+            Sign out
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
 
 
 const firebaseConfig = {
@@ -16,6 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth()
 const db = getFirestore(app)
+const storage = getStorage()
 const provider = new GoogleAuthProvider();
 
-export { db,auth, provider }
+export { db,auth, provider, storage }
